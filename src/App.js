5bf842aa8c69f169ca3b82f6c62026193b1660e1
TedOnyango <teddonyango@gmail.com>
2022-04-09 09:21:21 +0300

    1 file modified, 1 added

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import React from "react";
 import "./style.css";
 
 import NavBar from "./components/NavBar/NavBar";
+import TestRequestRegister from "./components/TestRequestRegister/TestRequestRegister"
+import Login from "./components/Login/Login";
 
 export default function App() {
   return (
     <div>
       <NavBar />
+      {/* <TestRequestRegister /> */}
+      <Login />
     
     </div>
   );
