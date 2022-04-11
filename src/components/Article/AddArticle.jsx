bb04750e0b87408822fe48af19c6548e3a2101da
TedOnyango <teddonyango@gmail.com>
2022-04-11 10:00:10 +0300

    1 file modified

--- src/components/Article/AddArticle.jsx
+++ src/components/Article/AddArticle.jsx
@@ -1,10 +1,12 @@
+import ka from "date-fns/locale/ka";
 import React,  { useState, useEffect } from "react";
 
 export default function AddArticle () {
-  const LOCAL_STORAGE_KE = "articles";
+  const LOCAL_STORAGE_KEY = "articles";
   const [articleName, setArticleName] = useState('');
   const [compoundType, setCompoundType] = useState('');
   const [department, setDepartment] = useState('');
+  const [articles, setArticles] = useState([]);
 
 
 
@@ -17,16 +19,27 @@ export default function AddArticle () {
    
   const addArticle = (e) => {
     e.preventDefault();
-   
-    
+    let articleObject = {
+      articleName: articleName,
+      compoundType: compoundType, 
+      department: department
+   }
+   setArticles(articleObject)
+   console.log(articles)
+   window.localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(articles))
+   setArticleName('');
+   setCompoundType('');
+   setDepartment('');
+
   }
+  
   // useEffect(() => {
   //   window.localStorage.setItem('article', JSON.stringify(article));
   //  }, [article]);
 
   return (
     <div>
-      <h1>This is Article</h1>
+      <h1>Add Article</h1>
       <form className="container">
   <label>
     Article Name:
@@ -38,7 +51,7 @@ export default function AddArticle () {
   </label>
   <label>
     Department:
-    <input type="text" name="department" onChange={(e) => setDepartment(e.target.value)}/>
+    <input type="text" name="department" value={department} onChange={(e) => setDepartment(e.target.value)}/>
   </label>
  
   <input onClick={addArticle}type="submit" value="Add"/>
