5bf38b58ffdd368f039da6a458fa338ec491f82d
TedOnyango <teddonyango@gmail.com>
2022-07-21 09:59:42 +0300

    1 file modified, 1 added, 1 deleted

--- src/components/TestReport/HeaderComponent.js
+++ src/components/TestReport/HeaderComponent.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import './TestReport.css';
 
-
 const HeaderComponent = () => {
   return (
     <label className="header-container">
       <div className="conpany-detials">
-        <img />
+        <img src='' alt="company-logo" />
         <p>Umoja Rubber Products Limited</p>
         <p>P. O. Box 452-80109 MTWAPA </p>
         <p>Mombasa-Malindi Highway, Kikambala</p>
