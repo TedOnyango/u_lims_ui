874eef6c1404f5ad93874cec8da3a46d5abddf4a
TedOnyango <teddonyango@gmail.com>
2022-07-21 09:52:10 +0300

    1 binaries uploaded

--- src/components/TestReport/HeaderComponent.js
+++ src/components/TestReport/HeaderComponent.js
@@ -1,10 +1,36 @@
 import React from 'react';
+import './TestReport.css';
+
 
 const HeaderComponent = () => {
   return (
-    <div>
-      <h1>This is header</h1>
+    <label className="header-container">
+      <div className="conpany-detials">
+        <img />
+        <p>Umoja Rubber Products Limited</p>
+        <p>P. O. Box 452-80109 MTWAPA </p>
+        <p>Mombasa-Malindi Highway, Kikambala</p>
+        <p>Tel.: +254 722 204 994</p>
+        <p>www.umojaafrica.com</p>
+        <p>Umoja Rubber Products Ltd is an
+ISO 9001:2015 Certified Company</p>
+
     </div>
+    <hr></hr>
+    <div className="lab-report-text">
+        <p>Laboratory Test Repor</p>
+    </div>
+    <hr></hr>
+
+    <div className="lab-detials">
+        <p>Quality Control Laboratory</p>
+        <p>Laboratory@umojarubber.comy</p>
+        <p>Doc Ref: URPL/QMSFM/QCLAB/8.6</p>
+        <p>Version: 1.2</p>
+        <p>Page: 1 of 1</p>
+
+    </div>
+    </label>
   )
 
 };
