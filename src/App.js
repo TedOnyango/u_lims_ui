eb659451604348c9f8f3e7d1a66673466571525a
TedOnyango <teddonyango@gmail.com>
2022-07-21 09:24:47 +0300

    1 file modified, 1 added

--- src/App.js
+++ src/App.js
@@ -4,14 +4,16 @@ import "./style.css";
 import NavBar from "./components/NavBar/NavBar";
 import TestRequestRegister from "./components/TestRequestRegister/TestRequestRegister";
 import DensityChart from "./components/Analytics/DensityChart";
+import HeaderComponent from "./components/TestReport/HeaderComponent"
 
 export default function App() {
   return (
     <div>
       <NavBar />
       {/* <TestRequestRegister /> */}
-      <DensityChart />
+      {/* <DensityChart /> */}
       {/* <Login /> */}
+      <HeaderComponent />
     
     </div>
   );
