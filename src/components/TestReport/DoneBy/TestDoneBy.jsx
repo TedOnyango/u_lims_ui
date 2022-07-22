16e1837fc9d4cb26c38950c28457dc8e1c03ce93
TedOnyango <teddonyango@gmail.com>
2022-07-22 17:01:46 +0300

    3 files modified, 2 added

--- src/components/TestReport/DoneBy/TestDoneBy.jsx
+++ src/components/TestReport/DoneBy/TestDoneBy.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
+import "./DoneBy.css"
 
 const TestDoneBy = () => {
   return(
     <div>
-      <label>
+      <label className="done-by-container">
         <p>Test done by: </p>
         <p>Sign: </p>
         <p>Date: </p>
         </label>
-
     </div>
   )
 
--- src/components/TestReport/Footer/Footer.jsx
+++ src/components/TestReport/Footer/Footer.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import "./Footer.css"
 
 const Footer = () => {
   return(
     <div>
         <hr></hr>
-        <div>
+        <div className="footer-container">
         <p>The results contained herein are only applicable to the specific sample(s) with the identified sample reference number, and the particular test parameters tested as indicated above</p>
     </div>
     </div>
