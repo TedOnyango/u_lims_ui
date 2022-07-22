9249339eb4d94f3795ee06055a73194bf027cadd
TedOnyango <teddonyango@gmail.com>
2022-07-22 09:04:22 +0300

    2 files modified

--- src/components/TestReport/HeaderComponent.js
+++ src/components/TestReport/HeaderComponent.js
@@ -13,23 +13,23 @@ const HeaderComponent = () => {
         <p id="details-text">P. O. Box 452-80109 MTWAPA </p>
         <p id="details-text">Mombasa-Malindi Highway, Kikambala</p>
         <p id="details-text">Tel.: +254 722 204 994</p>
-        <p id="details-text">www.umojaafrica.com</p>
+        <p id="details-text"><a className="umoja-link"><u>www.umojaafrica.com</u></a></p>
         <p id="details-text">Umoja Rubber Products Ltd is an
 ISO 9001:2015 Certified Company</p>
 
     </div>
     <hr></hr>
     <div className="lab-report-text">
-        <p>Laboratory Test Repor</p>
+        <p className="lab-report-text">Laboratory Test Report</p>
     </div>
-    <hr></hr>
+    <hr className="hr2"></hr>
 
-    <div className="lab-detials">
-        <p>Quality Control Laboratory</p>
-        <p>Laboratory@umojarubber.comy</p>
-        <p>Doc Ref: URPL/QMSFM/QCLAB/8.6</p>
-        <p>Version: 1.2</p>
-        <p>Page: 1 of 1</p>
+    <div className="lab-details">
+        <p id="lab-detials-text">Quality Control Laboratory</p>
+        <p id="lab-detials-text">Laboratory@umojarubber.comy</p>
+        <p id="lab-detials-text">Doc Ref: URPL/QMSFM/QCLAB/8.6</p>
+        <p id="lab-detials-text">Version: 1.2</p>
+        <p id="lab-detials-text">Page: 1 of 1</p>
 
     </div>
     </label>
